96bf5fd08597f30a94d78b221d6baca9b49dbe5e
bk167465 <bk167465@gmail.com>
2024-04-02 16:55:54 +0530

    Added Go Bookstore

---
+++ Go-bookstore/cmd/main/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"github.com/bk167465/Go_Tutorial/Go-bookstore/pkg/routes"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func main(){
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+	fmt.Println("Server listening on PORT 9010")
+}
